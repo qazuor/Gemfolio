6bd4dcc20eb38f850e1acdf711b4255c66fe8372
qazuor <qazuor@gmail.com>
2025-11-27 14:33:21 -0300

    feat(db): export query helpers from main package index
    Add queries re-export to main index.ts for simplified imports.

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,8 +1,8 @@
 // Database client
-
 export type { Database } from './client';
 export { db, schema } from './client';
-
+// Query exports
+export * from './queries';
 // Schema exports
 export * from './schema';
 
